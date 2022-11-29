35467739b142b52e52faacaa555fb220ee17e4f6
ferTaipi <fernandoparadoja@gmail.com>
2022-11-29 16:42:22 -0500

    29noviembre

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,12 @@ import React from 'react';
 const Home = () => {
     return (
         <div>
-            este es mi home
+            <div>
+                <div className='home-content'>
+                    <h2>DEVIF</h2>
+                    <p>La Unidad Nacional de Investigación Contra la Violencia de Género, Mujer o Miembros del Núcleo familiar, fue inicialmete creado como Departamento de Violencia Intrafamiliar</p>
+                </div>
+            </div>
         </div>
     );
 };
